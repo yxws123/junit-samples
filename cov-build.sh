c90383bd61553fbfd8c347d00acb51b81d6008af
Shuo Wu <ouhsuw@gmail.com>
2012-01-01 01:39:37 +0800

    modified:   cov-build.sh

--- cov-build.sh
+++ cov-build.sh
@@ -6,7 +6,7 @@
 #export OUTPUT=/home/zozoh/nutz/output
 #export PROJECT_HOME=/home/zozoh/workspace/svn/nutz/nutz.trunk
 
-HOME=/home/shuo
+HOME=/home/shuo00
 
 export JAVA_HOME=$HOME/apps/jdk
 export ANT_HOME=$HOME/apps/ant
