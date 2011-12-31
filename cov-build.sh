2bea9a9f81373fb767c07374410c5b59fc345133
Shuo Wu <ouhsuw@gmail.com>
2012-01-01 01:06:21 +0800

    modified:   cov-build.sh

--- cov-build.sh
+++ cov-build.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 
+HOME=/home/shuo
+
 #export TOMCAT_HOME=/home/zozoh/opt/tomcat6
 #export JAVA_HOME=/home/shuo/apps/jdk
 #export DEPS=/home/zozoh/nutz/deps/core
 #export OUTPUT=/home/zozoh/nutz/output
 #export PROJECT_HOME=/home/zozoh/workspace/svn/nutz/nutz.trunk
 
-export JAVA_HOME=/home/shuo/apps/jdk
-export ANT_HOME=/home/shuo/apps/ant
+export JAVA_HOME=$HOME/apps/jdk
+export ANT_HOME=$HOME/apps/ant
 export PATH=$JAVA_HOME/bin:$JAVA_HOME/jre/bin:$ANT_HOME/bin:$PATH
 
-cd /home/shuo/workspace/junit-samples
+
+cd $HOME/workspace/junit-samples
 
 ant -buildfile build-offline-linux.xml clean emma all
